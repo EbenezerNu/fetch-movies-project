8ab291efa933562e5f57f692ce45d39469d0c7b7
Catalyst546 <37144172+Catalyst546@users.noreply.github.com>
2023-02-10 11:31:57 +0000

    Finally implemented the fetching of movies

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -19,11 +19,21 @@ function App() {
   //   },
   // ];
   const [movieList, setMovieList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const btnRef = useRef();
   const fetchMoviesHandler = () => {
+    if (loading === true) return;
     console.log("Fetching movies ...");
+    btnRef.current.disabled = true;
+    setLoading(true);
+    setError(false);
     fetch("https://swapi.dev/api/films")
       .then((response) => {
         console.log("Received response ...");
+        if (!response.ok) {
+          throw Error("Failed to fetch movies");
+        }
         return response.json();
       })
       .then((data) => {
@@ -38,19 +48,43 @@ function App() {
               releaseDate: movie.release_date,
             };
           });
+          setError(false);
+          setLoading(false);
           setMovieList(movies);
         }
+      })
+      .catch(() => {
+        console.log("Something went wrong");
+        setError(true);
+        setLoading(false);
       });
+    btnRef.current.disabled = false;
   };
+
+  // useEffect(() => {
+  //   console.log("Button has been clicked");
+  // }, [btnRef?.current?.disabled]);
+  let content = <p></p>;
+  if (loading) {
+    content = <p>Loading ...</p>;
+  } else if (error) {
+    content = <p>Something went wrong</p>;
+  } else if (movieList.length > 0) {
+    content = <MoviesList movies={movieList} />;
+  } else if (movieList.length === 0) {
+    content = <p>No movie found</p>;
+  } else {
+    content = <p>Oops</p>;
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler} ref={btnRef}>
+          Fetch Movies
+        </button>
       </section>
-      <section>
-        <MoviesList movies={movieList} />
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
